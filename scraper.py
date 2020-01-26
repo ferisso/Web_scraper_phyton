4d0ff2b16578bd790db0bb79c37cae3f8088b542
Fernando Nogueira Risso <f.risso6@hotmail.com>
2020-01-25 21:21:01 -0300

    Update scrpper.py

--- scraper.py
+++ scraper.py
@@ -6,7 +6,9 @@ headers = {
     "user-agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36'
 }
 page = requests.get(url, headers = headers)
-
 soup = BeautifulSoup(page.content, 'html.parser')
+title = soup.find(id = "productTitle").get_text()
+price = soup.find(id="priceblock_ourprice").get_text()
 
-print(soup.prettify())
+print(title.strip())
+print(price.strip())

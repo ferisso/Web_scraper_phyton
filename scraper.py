3aac469a4e6abd95a09de944ca18ecfe77f8cc2c
Fernando Nogueira Risso <f.risso6@hotmail.com>
2020-01-27 00:28:22 -0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 'https://www.amazon.com.br/Smartwatch-Xiaomi-Preto-Original-Lacrado/dp/B07SNG23JW/ref=sr_1_1?__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=JHDJAZ33OSC4&keywords=mi+nand+4&qid=1579994931&sprefix=mi+namd%2Caps%2C303&sr=8-1'
+url = 'https://www.amazon.com.br/Monitor-LG-25UM58-P-AWZ-LED-25/dp/B01AWG4S4K/ref=lp_16364756011_1_1?s=computers&ie=UTF8&qid=1580095560&sr=1-1'
 headers = {
     "user-agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36'
 }
